ba1a89ab9f1066560c212721314acb68085c984b
RafaelHDev <rhsouzasilva@gmail.com>
2019-08-12 08:20:00 -0300

    Add files via upload

---
+++ cores.py
@@ -0,0 +1,30 @@
+"""
+Template: \033[_;_;_m]
+Exemplo: \033[0;33;44m (estilo normal, cor amarelo e fundo azul)
+
+Style
+== 0 -> Normal
+== 1 -> Negrito
+== 4 -> Sublinhado
+== 7 -> Inverte as configurações
+
+Cor
+== 30 -> Branco
+== 31 -> Vermelho
+== 32 -> Verde
+== 33 -> Amarelo
+== 34 -> Azul
+== 35 -> Lilás
+== 36 -> Azul Ciano
+== 37 -> Cinza
+
+Background
+== 40 -> Branco
+== 41 -> Vermelho
+== 42 -> Verde
+== 43 -> Amarelo
+== 44 -> Azul
+== 45 -> Lilás
+== 46 -> Azul Ciano
+== 47 -> Cinza
+"""
